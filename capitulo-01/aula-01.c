126c49e73777acb5fbe8cde05c9a1710295772ff
Feliprow <felipeneresvieira@gmail.com>
2025-09-13 21:27:48 -0300

    Primeiro programa em C + exercício inicial

---
+++ capitulo-01/aula-01.c
@@ -0,0 +1,28 @@
+//  Primeiro programa em C !
+
+//  comentario de somente uma linha
+
+/*
+    comentário de várias linhas
+*/
+
+#include <stdio.h>
+
+/* A linguagem C é uma linguagem modular (fragmentada) então precisamos incluir as bibliotecas que iremos usar no programa.
+
+    h -> cabeçalho
+*/
+
+
+// toda função começa na main, é o nosso ponto principal de programa.
+
+// int retorna o valor inteiro da função "main"
+int main () {
+
+    printf("Meu Primeiro programa em C!\n");
+    // printf é uma função de output de biblioteca
+
+
+    return 0;
+    // caso se tudo ocorrer bem ele retornará 0
+}
---
+++ capitulo-01/exercicios/ex-01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+
+
+    printf("Felipe Neres vieira\n");
+    printf("Andreza\n");
+    printf("Nick & Meg\n");
+
+
+    return 0;
+}
